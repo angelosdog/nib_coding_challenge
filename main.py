5f8e0158d625cf320dbd607da7614a9a1cf75119
angelosdog <angelo.gt@hotmail.com>
2022-09-16 13:03:37 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from Product import Product
 
 inventory = []
 orders = []
-unfulfilledOrderIds = []
 
 # Assumed to be have already been implemented, as per the spec
 def restock():
@@ -20,10 +19,16 @@ def restockProduct(productId):
     # this would probably be used in the restock() method, but only restocks a single product
     return
 
-def fulfillOrder(order):
-    productsToBuy = []
+# takes in a single 'order' and adds its id to 'unfulfilledOrderIds' if it cannot be fulfilled
+# checks that each item being ordered is in stock
+# marks unfulfillable orders as Unfulfillable
+# fulfills fulfillable orders
+def fulfillOrder(order, unfulfilledOrderIds):
     # check that the order can be fulfilled
     for orderProduct in order.items:
+        # in case the shopping cart has a weird value
+        if orderProduct['quantity'] <= 0:
+            break
         for inventoryProduct in inventory:
             # find the product in the inventory
             if orderProduct['productId'] == inventoryProduct.productId:
@@ -31,22 +36,30 @@ def fulfillOrder(order):
                 if inventoryProduct.quantityOnHand < orderProduct['quantity'] or inventoryProduct.quantityOnHand <= 0:
                     restockProduct(orderProduct['productId'])
                     order.status = OrderStatus.Unfulfillable.name
-                    # continue the loop in case other products need restocking
+                    # reduce inventory to 0, and reduce order quantity by the current stock left
+                    orderProduct['quantity'] = orderProduct['quantity'] - inventoryProduct.quantityOnHand
+                    inventoryProduct.quantityOnHand = 0
+                # product is in stock
                 else:
-                    # save the amount of products to buy so we can deduct it from the inventory if the order can be fulfilled
-                    productsToBuy.append((inventoryProduct, orderProduct['quantity']))
+                    # deduct from inventory quantityOnHand, and reduce quantity left to buy to 0
+                    inventoryProduct.quantityOnHand = inventoryProduct.quantityOnHand - orderProduct['quantity']
+                    orderProduct['quantity'] = 0
+                break
     # fulfill the order if it is Pending
     if order.status == OrderStatus.Pending.name:
-        for i in productsToBuy:
-            i[0].quantityOnHand = i[0].quantityOnHand - i[1]
-        # mark order as fulfulled
         order.status = OrderStatus.Fulfilled.name
     else:
         unfulfilledOrderIds.append(order.orderId)
-        
-def processOrders():
-    for order in orders:
-        fulfillOrder(order)
+
+# takes in 'orderIds' to process and returns an array of all unfulfilled orderIds
+def processOrders(orderIds):
+    unfulfilledOrderIds = []
+    for orderId in orderIds:
+        for order in orders:
+            if int(orderId) == order.orderId:
+                fulfillOrder(order, unfulfilledOrderIds)
+                break
+    return unfulfilledOrderIds
         
 # # for testing
 # def printInventory():
@@ -57,6 +70,7 @@ def processOrders():
 #         print("reorderThreshold:", i.reorderThreshold)
 #         print("reorderAmount:", i.reorderAmount)
 #         print("deliveryLeadTime:", i.deliveryLeadTime)
+#         print()
 
 # # for testing
 # def printOrders():
@@ -65,31 +79,43 @@ def processOrders():
 #         print("status:", o.status)
 #         print("dateCreated:", o.dateCreated)
 #         printOrderProducts(o)
+#     print()
 
 # # for testing
 # def printOrderProducts(order):
 #     for i in order.items:
-#         print("orderId", i['orderId'])
-#         print("productId", i['productId'])
-#         print("quantity", i['quantity'])
-#         print("costPerItem", i['costPerItem'])
+#         print("   orderId", i['orderId'])
+#         print("   productId", i['productId'])
+#         print("   quantity", i['quantity'])
+#         print("   costPerItem", i['costPerItem'])
+#         print("   ##################")
 
 class main:
     f = open('data.json')
     data = json.load(f)
     f.close()
-    
+
+    # create inventory array of products
     for p in data['products']:
         currProduct = Product(p['productId'], p['description'], p['quantityOnHand'], p['reorderThreshold'], p['reorderAmount'], p['deliveryLeadTime'])
         inventory.append(currProduct)
 
+    allOrderIds = []
+    # create array of orders
     for o in data['orders']:
         currOrder = Order(o['orderId'], o['status'], o['dateCreated'], o['items'])
+        allOrderIds.append(str(o['orderId']))
         orders.append(currOrder)
 
+    print("Enter comma-delimited productIds to process. Leave blank to process all orders")
+    orderIdsToProcess = input()
+    if orderIdsToProcess == '':
+        orderIdsToProcess = ",".join(allOrderIds)
+    print("Processing", orderIdsToProcess)
+
     restock()
-    processOrders()
+    unfulfilledOrderIds = processOrders(orderIdsToProcess.split(","))
     # for testing
     # printInventory()
     # printOrders()
-    print(unfulfilledOrderIds)
+    print("Unfulfillable orderIds:", unfulfilledOrderIds)
